c0cbd03a92ddb4a9efca8647e1071c407ffd3672
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2024-01-01 18:17:48 -0300

    Implementando o Config Module

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { EnvConfig } from './env-config.interface'
+import { ConfigService } from '@nestjs/config'
 
 @Injectable()
-export class EnvConfigService {}
+export class EnvConfigService implements EnvConfig {
+  //TODO: rodar essa linha de baixo no gpt pra entender oq faz
+  constructor(private configService: ConfigService) {}
+
+  getAppPort(): number {
+    return Number(this.configService.get<number>('PORT'))
+  }
+
+  getNodeEnv(): string {
+    return this.configService.get<string>('NODE_ENV')
+  }
+}
