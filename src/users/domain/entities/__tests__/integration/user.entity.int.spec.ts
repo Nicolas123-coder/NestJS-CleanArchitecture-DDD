468265b6e4b347bca684971b54e1df41b6e978c7
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2024-01-02 23:10:01 -0300

    Criando testes de integração - entidade User com EntityValidationError

---
+++ src/users/domain/entities/__tests__/integration/user.entity.int.spec.ts
@@ -0,0 +1,30 @@
+import { EntityValidationError } from '@/shared/domain/errors/validation-error'
+import { UserEntity, UserProps } from '../../user.entity'
+import { UserDataBuilder } from '@/users/domain/testing/helpers/user-data-builder'
+
+describe('UserEntity integration tests', () => {
+  describe('Constructor method', () => {
+    it('Should throw an error when creating a user with invalid name', () => {
+      let props: UserProps = {
+        ...UserDataBuilder({}),
+        name: null,
+      }
+
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        name: '',
+      }
+
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+
+      props = {
+        ...UserDataBuilder({}),
+        name: 'a'.repeat(256),
+      }
+
+      expect(() => new UserEntity(props)).toThrow(EntityValidationError)
+    })
+  })
+})
