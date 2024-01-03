25f55c85efa2253db9006912e9676449eddff902
Nicolas de Barros <nicolasdbarros@yahoo.com.br>
2024-01-03 20:55:59 -0300

    Criando In Memory Repository

---
+++ src/shared/domain/repositories/in-memory.repository.ts
@@ -0,0 +1,43 @@
+import { RepositoryInterface } from './repository-contracts'
+import { NotFoundError } from '../errors/not-found-error'
+import { Entity } from '../entities/entity'
+
+export abstract class InMemoryRepository<E extends Entity>
+  implements RepositoryInterface<E>
+{
+  items: E[] = []
+
+  async insert(entity: E): Promise<void> {
+    this.items.push(entity)
+  }
+
+  async findById(id: string): Promise<E> {
+    // [INFO] essa transformação com crase transforma automaticamente o id pra uma string
+    const _id = `${id}`
+    const entity = this.items.find(item => item.id === _id)
+
+    if (!entity) {
+      throw new NotFoundError('Entity Not Found')
+    }
+
+    return entity
+  }
+
+  async findAll(): Promise<E[]> {
+    return this.items
+  }
+
+  async update(entity: E): Promise<void> {
+    const _entity = await this.findById(entity.id)
+    const index = this.items.findIndex(item => item.id === _entity.id)
+
+    this.items[index] = entity
+  }
+
+  async delete(id: string): Promise<void> {
+    const _entity = await this.findById(id)
+    const entityIndex = this.items.findIndex(item => item.id === _entity.id)
+
+    this.items.slice(entityIndex, 1)
+  }
+}
